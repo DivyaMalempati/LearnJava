47afb35781c99eac57e998d8dfac79ac5d185ec4
DivyaMalempati <daggubati.divya1@gmail.com>
2019-05-17 15:05:00 -0700

    Update Practice Arrays
    Finding the Sum,Product of Array

--- src/practiceArrays.java
+++ src/practiceArrays.java
@@ -13,7 +13,27 @@ class practiceArrays {
 		findElementinArray(sc, array);
 		typesOfNumbers(array);
 		copyInReverseOrder(array,reverseArray);
+		sumOfArray(array);
+		productofArray(array);
 		sc.close();
+	}
+
+
+	private static void productofArray(int[] array) {
+		int product=1;
+		for(int i=0;i<array.length;i++) {
+			product*=array[i];
+		}
+		System.out.println("The Product of Array :"+product);
+	}
+
+
+	private static void sumOfArray(int[] array) {
+		int sum=0;
+		for(int i=0;i<array.length;i++) {
+			sum+=array[i];
+		}
+		System.out.println("The sum of the Array : "+sum);
 	}
 
 
@@ -26,15 +46,13 @@ class practiceArrays {
 
 	private static void printArray(int[] array) {
 		System.out.print("The Elements in the Array are : [");
-		for (int i : array) {
-			System.out.print(i + ",");
-
-		}
-		System.out.print("]");
-		System.out.println();
-		// for(int i=0;i<array.length;i++) {
-		// System.out.println("Array["+i+"] is :"+array[i]);
-		// }
+		 for(int i=0;i<array.length;i++) {
+			 System.out.print(array[i]);
+			 if(i!=array.length-1) {
+				 System.out.print(",");
+			 }
+		 }
+		 System.out.println("]");
 	}
 
 	private static void findElementinArray(Scanner sc, int[] array) {
@@ -44,6 +62,8 @@ class practiceArrays {
 		for (int i = 0; i < array.length; i++) {
 			if (elementToFind == array[i]) {
 				counter++;
+				//System.out.println(counter);
+				break;
 			}
 		}
 		if (counter > 0) {
@@ -62,10 +82,10 @@ class practiceArrays {
 			if (array[i] < 0) {
 				neg++;
 			}
-			if (array[i] / 2 == 0) {
+			if (array[i] % 2 == 0) {
 				even++;
 			}
-			if (array[i] / 2 != 0) {
+			if (array[i] % 2 != 0) {
 				odd++;
 			}
 			if (array[i] == 0) {
