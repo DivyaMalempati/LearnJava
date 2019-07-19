25463ac75b17198e4ef9d02ea5867b3fc0beabf6
DivyaMalempati <daggubati.divya1@gmail.com>
2019-07-18 22:10:55 -0700

    findUniqueNumbers with allocating any memory
    find unique numbers in an array

---
+++ src/FindUniqueNumbers.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+
+class FindUniqueNumbers {
+	private static final String DELIMITER_BETWEEN_NUMBERS = ",";
+
+	public static void main(String[] args) {
+		int numberOfTestCases = 0;
+		Scanner scanner = new Scanner(System.in);
+		numberOfTestCases = Integer.parseInt(scanner.nextLine());
+		for (int initialTestCase = 0; initialTestCase < numberOfTestCases; initialTestCase++) {
+			int arraySize;
+			String lineOfNumbers = scanner.nextLine();
+			String[] inputNumberStringArray = lineOfNumbers.split(DELIMITER_BETWEEN_NUMBERS);
+
+			arraySize = inputNumberStringArray.length;
+			int[] array = new int[arraySize];
+			for (int currentIndex = 0; currentIndex < arraySize; currentIndex++) {
+				array[currentIndex] = Integer.parseInt(inputNumberStringArray[currentIndex]);
+			}
+			printArray(array);
+			int[] uniqueArray = findUniqueNumbersWithMemory(array);
+			printArray(uniqueArray);
+		}
+		scanner.close();
+	}
+
+	private static int[] findUniqueNumbersWithMemory(int[] array) {
+		int[] uniqueArray = new int[array.length];
+		int uniqueArrayActualSize = 0;
+		for (int currentIndex = 0; currentIndex < array.length; currentIndex++) {
+			boolean isUnique = true;
+			for (int previousIndex = 0; previousIndex < currentIndex; previousIndex++) {
+				if (array[previousIndex] == array[currentIndex]) {
+					isUnique = false;
+					break;
+				}
+			}
+			if (isUnique) {
+				uniqueArray[uniqueArrayActualSize] = array[currentIndex];
+				uniqueArrayActualSize++;
+			}
+		}
+
+		return uniqueArray;
+	}
+
+	private static void printArray(int[] array) {
+		System.out.print("[");
+		for (int i : array) {
+			System.out.print(i + DELIMITER_BETWEEN_NUMBERS);
+		}
+		System.out.println("]");
+	}
+}
