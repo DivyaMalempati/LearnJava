8e7733ec8663e1b06adbb08d4bb28312928a5f1b
DivyaMalempati <daggubati.divya1@gmail.com>
2019-08-07 23:44:33 -0700

    Find Leader in An Array
    An element is leader if it is greater than all the elements to its right side. And the rightmost element is always a leader.

--- src/FindLeadersInAnArray.java
+++ src/FindLeadersInAnArray.java
@@ -19,10 +19,26 @@ public class FindLeadersInAnArray {
 		return true;
 	}
 
+	public static void findLeadersInArrayOptimal(int[] array) {
+		int maxElement = array[array.length - 1];
+		System.out.println(maxElement);
+		for (int lastIndex = array.length - 1; lastIndex >= 0; lastIndex--) {
+			if (maxElement < array[lastIndex]) {
+				maxElement = array[lastIndex];
+				System.out.println(maxElement);
+			}
+		}
+	}
+
 	@Test
 	public void findLeader_Test() {
 		findLeadersInAnArray(new int[] { 16, 17, 4, 3, 5, 2 });
 		findLeadersInAnArray(new int[] { 20, 19, 18, 17, 16, 15, 14 });
 		findLeadersInAnArray(new int[] { 1, 2, 3, 4, 5, 6 });
+	}
+
+	@Test
+	public void findLeader_Optimal_Test() {
+		findLeadersInArrayOptimal(new int[] { 16, 17, 4, 3, 5, 2 });
 	}
 }
