bac6d2b67d41c46a38b018c9eed7f6910083cd8e
DivyaMalempati <daggubati.divya1@gmail.com>
2019-08-18 10:24:43 -0700

    Implementation of add an integer to an integer array

---
+++ src/AddToArrayFormInteger.java
@@ -0,0 +1,63 @@
+import static org.junit.Assert.assertEquals;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+public class AddToArrayFormInteger {
+	public List<Integer> addToArrayForm(int[] A, int K) {
+		StringBuilder sb = new StringBuilder();
+		for (Integer integer : A) {
+			sb = sb.append(integer);
+		}
+		BigInteger number = new BigInteger(sb.toString());
+		BigInteger KBI = new BigInteger("" + K);
+		number = number.add(KBI);
+		LinkedList<Integer> list = new LinkedList<>();
+		while (!number.equals(BigInteger.valueOf(0))) {
+			list.addFirst(number.mod(BigInteger.valueOf(10)).intValue());
+			number = number.divide(BigInteger.valueOf(10));
+		}
+		if(list.isEmpty()) {
+			list.add(0);
+		}
+		return list;
+	}
+
+	public List<Integer> addToArrayIntForm(int[] array, int K) {
+		StringBuilder sb = new StringBuilder();
+		for (int i : array) {
+			sb.append(i);
+		}
+		Integer value = new Integer(sb.toString());
+		value += K;
+		LinkedList<Integer> list = new LinkedList<>();
+		while (value > 0) {
+			int div = value % 10;
+			list.addFirst(div);
+			value /= 10;
+		}
+		return list;
+	}
+
+	@Test
+	public void addIntegerTest() {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(9);
+		assertEquals(list, addToArrayIntForm(new int[] { 1, 2, 3 }, 6));
+	}
+
+	@Test
+	public void addBigIntegerTest() {
+		List<Integer> list = new ArrayList<>();
+		list.add(1);
+		list.add(2);
+		list.add(9);
+		assertEquals(list, addToArrayForm(new int[] { 1, 2, 3 }, 6));
+	}
+}
