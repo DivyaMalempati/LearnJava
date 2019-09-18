68af7c80283f5e52f6781a8a7a61bb62d7e57a68
DivyaMalempati <daggubati.divya1@gmail.com>
2019-09-18 13:01:15 -0700

    Reverse a Linkedlist
    reverse between m and n
    reverse list till n

--- src/com/divya/learnjava/ReverseALinkedList.java
+++ src/com/divya/learnjava/ReverseALinkedList.java
@@ -1,6 +1,7 @@
 package com.divya.learnjava;
 
 import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,14 @@ import org.junit.jupiter.api.Test;
 import com.divya.learnjava.LinkedList.LinkedListNode;
 
 class ReverseALinkedList {
+	public class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
 
 	public static LinkedListNode reverseLinkedList(LinkedListNode head) {
 		if (head == null) {
@@ -27,6 +36,109 @@ class ReverseALinkedList {
 		}
 
 		return currentNode;
+	}
+
+	public ListNode reverseList(ListNode head) {
+		ListNode currNode = head;
+		ListNode previousNode = null;
+		while (currNode != null) {
+			ListNode temp = currNode.next;
+			currNode.next = previousNode;
+			previousNode = currNode;
+			currNode = temp;
+		}
+		return previousNode;
+	}
+
+	private void printList(ListNode currNode) {
+		while (currNode != null) {
+			System.out.println(currNode.val);
+			currNode = currNode.next;
+		}
+	}
+
+	public ListNode reverseListBetween(ListNode head, int m, int n) {
+		ListNode currNode = head;
+		if (m == 1) {
+			return reverseListTillN(currNode, n - m + 1);
+		}
+		// ListNode reverseNode = new ListNode(-1);
+		int count = 1;
+		while (currNode != null) {
+
+			if (count == m - 1) {
+				currNode.next = reverseListTillN(currNode.next, n - m + 1);
+				break;
+			}
+			++count;
+			currNode = currNode.next;
+		}
+		return head;
+	}
+
+	private ListNode reverseListTillN(ListNode currNode, int count) {
+		ListNode previousNode = null;
+		ListNode oldCurrNode = currNode;
+		ListNode nextNode = currNode.next;
+		while (count-- > 0) {
+			nextNode = currNode.next;
+			currNode.next = previousNode;
+			previousNode = currNode;
+			currNode = nextNode;
+		}
+		oldCurrNode.next = nextNode;
+		return previousNode;
+	}
+
+	@Test
+	public void reverseListBetween_Test() {
+		ListNode node = new ListNode(10);
+		ListNode node1 = new ListNode(20);
+		node.next = node1;
+		ListNode node2 = new ListNode(30);
+		node1.next = node2;
+		ListNode node3 = new ListNode(40);
+		node2.next = node3;
+		ListNode node4 = new ListNode(50);
+		node3.next = node4;
+		ListNode reversedNode = reverseListBetween(node,1,1);
+		printList(reversedNode);
+		// ListNode reversedHead = reverseListTillN(node, 2);
+		// printList(reversedHead);
+	}
+
+	@Test
+	public void reverse_ListTest() {
+		ListNode node = new ListNode(10);
+		ListNode node1 = new ListNode(20);
+		node.next = node1;
+		ListNode node2 = new ListNode(30);
+		node1.next = node2;
+		ListNode node3 = new ListNode(40);
+		node2.next = node3;
+
+		ListNode reverseHeadNode = new ListNode(40);
+		ListNode node5 = new ListNode(30);
+		reverseHeadNode.next = node5;
+		ListNode node6 = new ListNode(20);
+		node5.next = node6;
+		ListNode node7 = new ListNode(10);
+		node6.next = node7;
+
+		ListNode actualReverseHeadNode = reverseList(node);
+		compareTwoLists(reverseHeadNode, actualReverseHeadNode);
+		actualReverseHeadNode = reverseList(null);
+		compareTwoLists(null, actualReverseHeadNode);
+	}
+
+	private void compareTwoLists(ListNode reverseHeadNode, ListNode actualReverseHeadNode) {
+		ListNode originalNode = reverseHeadNode, actualNode = actualReverseHeadNode;
+		for (; originalNode != null
+				&& actualNode != null; originalNode = originalNode.next, actualNode = actualNode.next) {
+			assertEquals(originalNode.val, actualNode.val);
+		}
+		assertNull(actualNode);
+		assertNull(originalNode);
 	}
 
 	@Test
